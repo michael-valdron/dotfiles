11f55c9ec1eb3e956845ac632f64cee5d73734ea
Michael Valdron <michael.valdron@gmail.com>
2019-08-11 02:00:53 -0400

    profiles.clj changed.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,3 @@
-{:user {:plugins [[cider/cider-nrepl "0.20.0"]
-                  [io.aviso/pretty "0.1.26"]]
-        :dependencies [[io.aviso/pretty "0.1.26"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.21.1"]
+                  [io.aviso/pretty "0.1.37"]]
+        :dependencies [[io.aviso/pretty "0.1.37"]]}}
