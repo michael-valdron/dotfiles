be8e5579e790a1e2961e65cde86adff27299c436
Michael Valdron <michael.valdron@gmail.com>
2019-08-11 02:13:00 -0400

    Fixed pretty plugin.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,3 @@
 {:user {:plugins [[cider/cider-nrepl "0.21.1"]
-                  [io.aviso/pretty "0.1.37"]]
-        :dependencies [[io.aviso/pretty "0.1.37"]]}}
+                  [io.aviso/pretty "0.1.21"]]
+        :dependencies [[io.aviso/pretty "0.1.21"]]}}
