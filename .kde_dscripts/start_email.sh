9da9c31abdd6916e5aae33838d847990ed36b970
Michael Valdron <michael.valdron@gmail.com>
2020-01-09 05:30:16 -0500

    kde autostart program commands for each workspace.

---
+++ .kde_dscripts/start_email.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Starts Emails
+kstart5 --desktop 4 --maximize $(firefox --new-window https://mail.google.com/mail/u/0/?tab=wm#inbox)
---
+++ .kde_dscripts/start_web.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Starts Web
+kstart5 --desktop 2 --maximize firefox
