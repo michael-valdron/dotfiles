0bda5817fa33081d33889b19ef4588fea5922547
Michael Valdron <michael.valdron@gmail.com>
2020-09-09 13:04:10 -0400

    remove sayid plugin.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,3 @@
 {:user {:plugins [[cider/cider-nrepl "0.21.1"]
-                  [com.billpiel/sayid "0.0.18"]
                   [io.aviso/pretty "0.1.21"]]
         :dependencies [[io.aviso/pretty "0.1.21"]]}}
