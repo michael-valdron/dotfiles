9b2ec8dbfdc3053ab6a090a3e8763ceff09a9a68
Michael Valdron <michael.valdron@gmail.com>
2021-02-01 20:31:44 -0500

    clj-time added globally.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[cider/cider-nrepl "0.21.1"]
                   [io.aviso/pretty "0.1.21"]]
-        :dependencies [[io.aviso/pretty "0.1.21"]]}}
+        :dependencies [[io.aviso/pretty "0.1.21"]
+                       [clj-time "0.15.2"]]}}
