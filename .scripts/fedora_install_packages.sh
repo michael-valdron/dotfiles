be8fff5edfd23fdde982af4cab4c82acb7c148df
Michael Valdron <michael.valdron@gmail.com>
2021-12-14 03:43:09 -0500

    Fixed issues with a few installs in fedora_install_packages.sh

--- .scripts/fedora_install_packages.sh
+++ .scripts/fedora_install_packages.sh
@@ -39,7 +39,7 @@ dnf -y update
 # Install packages
 dnf -y install neofetch cmatrix tmux htop ufw zsh gcc gcc-c++ curl make cmake go java-11-openjdk clojure code docker-ce docker-ce-cli containerd.io chromium \
     firefox flatpak keepassxc barrier gimp libreoffice calibre xournal clamav clamtk vlc sqlitebrowser p7zip p7zip-gui p7zip-plugins cheese @virtualization \
-    unzip wget libappindicator redhat-lsb-core google-cloud-sdk bridge-utils
+    unzip wget libappindicator redhat-lsb-core google-cloud-sdk bridge-utils openssl
 
 # Install Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -47,17 +47,18 @@ chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # Install Minikube
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm -o /tmp/minikube-latest.x86_64.rpm
+curl -L "https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm" -o /tmp/minikube-latest.x86_64.rpm
 rpm -Uvh /tmp/minikube-latest.x86_64.rpm
 
 # Install odo
-curl -L https://mirror.openshift.com/pub/openshift-v4/clients/odo/latest/odo-linux-amd64 -o /usr/local/bin/odo
+curl -L "https://mirror.openshift.com/pub/openshift-v4/clients/odo/latest/odo-linux-amd64" -o /usr/local/bin/odo
 chmod +x /usr/local/bin/odo
 
 # Install Waterfox
 curl -L "https://github.com/WaterfoxCo/Waterfox/releases/download/G4.0.4/waterfox-G4.0.4.en-US.linux-x86_64.tar.bz2" -o /tmp/waterfox.tar.bz2
-tar -xf /tmp/waterfox.tar.bz2
+tar -xf /tmp/waterfox.tar.bz2 -C /tmp
 mv /tmp/waterfox /opt/waterfox
+chmod +x /opt/waterfox/waterfox
 ln -s /opt/waterfox/waterfox /usr/bin/waterfox
 
 # Install Mailspring
@@ -67,12 +68,14 @@ rpm -i --quiet /tmp/mailspring.rpm
 # Install Etcher
 mkdir -p /opt/balena-etcher
 curl -L "https://github.com/balena-io/etcher/releases/download/v1.7.1/balenaEtcher-1.7.1-x64.AppImage" -o /opt/balena-etcher/balena-etcher.AppImage
+chmod +x /opt/balena-etcher/balena-etcher.AppImage
 ln -s /opt/balena-etcher/balena-etcher.AppImage /usr/bin/etcher
 
 # Install Zotero
 mkdir -p /opt/zotero
 curl -L "https://www.zotero.org/download/client/dl?channel=release&platform=linux-x86_64&version=5.0.96.3" -o /tmp/zotero.tar.bz2
-tar -xf /tmp/zotero.tar.bz2 -C /opt/zotero
+tar -xf /tmp/zotero.tar.bz2 -C /tmp
+mv /tmp/Zotero_linux-x86_64 /opt/zotero
 ln -s /opt/zotero/zotero.desktop /usr/local/share/applications/zotero.desktop
 
 # Install Gradle
@@ -81,6 +84,7 @@ curl -L "https://downloads.gradle-dn.com/distributions/gradle-7.0-bin.zip" -o /t
 unzip /tmp/gradle.zip -d /opt/gradle
 mv /opt/gradle/gradle-7.0/* /opt/gradle
 rm -rf /opt/gradle/gradle-7.0
+chmod +x /opt/gradle/bin/gradle
 ln -s /opt/gradle/bin/gradle /usr/bin/gradle
 
 # Install Leiningen
