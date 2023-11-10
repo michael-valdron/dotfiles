7712fd62b958a5037f8f789a8e9f4633d48bc816
Michael Valdron <14838510+michael-valdron@users.noreply.github.com>
2023-11-10 16:41:35 -0500

    io.aviso clojure plugin update

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
 {:user {:plugins [[cider/cider-nrepl "0.21.1"]
-                  [io.aviso/pretty "0.1.21"]]
-        :dependencies [[io.aviso/pretty "0.1.21"]
+                  [io.aviso/pretty "1.4.4"]]
+        :dependencies [[io.aviso/pretty "1.4.4"]
                        [clj-time "0.15.2"]]}}
